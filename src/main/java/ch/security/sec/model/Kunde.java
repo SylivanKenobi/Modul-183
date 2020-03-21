97fbe265cc5919f0dff0184985ab8b84e58c638e
Nicola <nicola.fricker@hotmail.com>
2020-03-21 11:34:51 +0100

    added kunde model

--- src/main/java/ch/security/sec/model/Kunde.java
+++ src/main/java/ch/security/sec/model/Kunde.java
@@ -1,4 +1,57 @@
 package ch.security.sec.model;
 
+@Entity
 public class Kunde {
+
+    @Id
+    @NotNull
+    private long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String postanschrift;
+    private String kreditkarte;
+
+    public Kunde(){
+        
+    }
+
+    public Kunde(long Id, String Name, String Postanschrift, String Kreditkarte){
+        this.id = Id;
+        this.name = Name;
+        this.postanschrift = Postanschrift;
+        this.kreditkarte = Kreditkarte;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostanschrift() {
+        return postanschrift;
+    }
+
+    public void setPostanschrift(String postanschrift) {
+        this.postanschrift = postanschrift;
+    }
+
+    public String getKreditkarte() {
+        return kreditkarte;
+    }
+
+    public void setKreditkarte(String kreditkarte) {
+        this.kreditkarte = kreditkarte;
+    }
 }
