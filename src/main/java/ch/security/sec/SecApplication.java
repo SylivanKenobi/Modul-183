4d6da58c686825cf2b0ab1dfe3761fc72684839f
s.gilgen <gilgen@puzzle.ch>
2020-03-21 10:37:45 +0100

    Initialize Spring

---
+++ src/main/java/ch/security/sec/SecApplication.java
@@ -0,0 +1,13 @@
+package ch.security.sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/security/sec/SecApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.security.sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
